6c4318f9274b890fe2b806873bf4454a0d54b687
Anamika Kafle <kafleanamika123@gmail.com>
2021-03-18 23:15:52 +0545

    second commit

---
+++ app/src/main/java/me/mahakagg/recyclerview/MainActivity.java
@@ -0,0 +1,70 @@
+package me.mahakagg.recyclerview;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    // LinkedList to hold the data
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int wordListSize = mWordList.size();
+
+                mWordList.addLast("+ Word " + wordListSize);
+
+                Objects.requireNonNull(mRecyclerView.getAdapter()).notifyItemInserted(wordListSize);
+
+                mRecyclerView.smoothScrollToPosition(wordListSize);
+            }
+        });
+
+
+        for (int i = 1; i < 21; i++) {
+            mWordList.add("Word " + i);
+        }
+
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        WordListAdapter mAdapter = new WordListAdapter(this, mWordList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
